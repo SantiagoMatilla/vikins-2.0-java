a02f51da610b22de137b6e6f4319e90250dfb1b5
Santiago <esematillajr@gmail.com>
2023-07-13 14:42:02 +0200

    saxon testeado

--- src/main/java/org/example/saxon/Saxon.java
+++ src/main/java/org/example/saxon/Saxon.java
@@ -3,7 +3,26 @@ package org.example.saxon;
 import org.example.soldier.Soldier;
 
 public class Saxon extends Soldier {
+    String msg;
+
     public Saxon(int health, int strength) {
         super(health, strength);
+    }
+
+    public int attack(){
+        return this.strength;
+    }
+
+    public void receiveDamage(int damage){
+        this.health -= damage;
+        if(health > 0){
+            this.msg = "A Saxon has received " + damage + " points of damage";
+        }else{
+            this.msg ="A Saxon has died in combat";
+        }
+    }
+
+    public String getMsg() {
+        return msg;
     }
 }
